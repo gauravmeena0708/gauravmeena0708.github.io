9fa4dd00b4b977ef65ea1448944cf6f1de203582
gauravmeena0708 <gauravmeena0708@gmail.com>
2022-06-09 16:22:08 +0530

    Update script4.js

--- concept/script4.js
+++ concept/script4.js
@@ -109,7 +109,7 @@ function getDiff(d1, d2, str, withbool=1) {
 		d2=interval2.length('Days');
 		days=(Y*365)+(M*30)+d2
 		console.log(Y, M, D,days, date3,d2)
-		
+		return days;
 	}
 	var diffUnits = 0;
 	if(!withbool){
