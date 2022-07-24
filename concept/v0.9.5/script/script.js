2667682e1bb3c9f57b73e0fa5620496cb5571d35
gauravmeena0708 <gauravmeena0708@gmail.com>
2022-07-24 20:16:24 +0530

    adding font size increase

--- concept/v0.9.5/script/script.js
+++ concept/v0.9.5/script/script.js
@@ -288,3 +288,26 @@ function validateService(service, dob){
 	}
 	return 1;
 }
+
+    $(document).ready(function () {
+      $('#increase').click(function () {
+        modifyFontSize('increase');
+      });
+      $('#decrease').click(function () {
+        modifyFontSize('decrease');
+      });
+      $('#reset').click(function () {
+        modifyFontSize('reset');
+      });
+      function modifyFontSize(action) {
+        var divElement = $('.demo');
+        var currentFontSize = parseInt(divElement.css('font-size'));
+        if (action == 'increase')
+          currentFontSize += 3;
+        else if (action == 'decrease')
+          currentFontSize -= 3;
+        else
+          currentFontSize = 12;
+        divElement.animate({ 'fontSize': currentFontSize }, 300);
+      }
+    });
