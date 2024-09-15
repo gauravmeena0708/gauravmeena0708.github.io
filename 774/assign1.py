96ab9b43000cd18a2242f8dbb15652cdcce4b650
Gaurav Meena <gauravmeena0708@gmail.com>
2024-09-15 15:55:23 +0530

    Update assign1.py

--- 774/assign1.py
+++ 774/assign1.py
@@ -1,1 +1,29 @@
-print("Hello World")
+#these are psudocodes for different algorithms
+def linear_simple(processed_data):
+  X, Y, U, X_test = processed_data
+  XTX = X.T @ (X * U)
+  XTY = X.T @ (Y * U)
+  w_star = np.linalg.solve(XTX, XTY)
+  Y_pred = X_test @ w_star
+
+def linear_regularised():
+  XTX_regularised = X.T @ X + best_lambda * I
+  XTY = X.T @ Y
+  w_final = linalg.solve(XTX_regularised, XTY)
+  Y_pred = X_test @ w_final
+
+def logistic_binary(X, Y):
+  W = np.random.randn(n_features)
+  sig = 1/ (1 + np.exp(-X@W))
+  loss  = -np.sum(Y * np.log(sig) + (1 - Y) * np.log(1 - sig))
+  W_final = gradient_decent(X, Y, W, learning_rate)
+  Y_pred = 1/ (1 + np.exp(-X@W_final)) >= 0.5
+
+def logistic_multi(X, Y):
+  W = np.random.randn(n_features, n_classes)
+  softmax = np.exp(X@W)/np.sum(np.exp(X@W))
+  loss  = sum( log(softmax where y==1)
+  W_final = gradient_decent(X, Y, W, learning_rate)
+  Y_pred = np.exp(X@W_final)/np.sum(np.exp(X@W_final))
+      
+
