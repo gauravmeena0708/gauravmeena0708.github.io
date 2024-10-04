cfabb929b7925ad3b7f681887debb8c9937e8ba4
Gaurav Meena <gauravmeena0708@gmail.com>
2024-10-04 21:52:05 +0530

    Update ai2.js

--- havannah/ai2.js
+++ havannah/ai2.js
@@ -240,7 +240,7 @@ class AIPlayer2 {
    get_opening_moves(state, valid_actions) {
     const size = this.size;
     const dim = state.length;
-    const corners = this.get_all_corners(dim);
+    const corners = this.getAllCorners(dim);
 
     // Define adjacent nodes logic for size 6 and other sizes
     const adjacent_nodes = {
